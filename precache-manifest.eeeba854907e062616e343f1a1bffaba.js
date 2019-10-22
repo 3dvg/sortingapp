80349139eef7800c10021653f35c80073cb4a052
edu <3dvgg1@gmail.com>
2019-10-22 20:16:59 +0200

    Updates

---
+++ precache-manifest.eeeba854907e062616e343f1a1bffaba.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "53afcd4a6c47e03bab22c70c543d4ff9",
+    "url": "/sortingapp/index.html"
+  },
+  {
+    "revision": "49bb9a2f9caa2c7a7f63",
+    "url": "/sortingapp/static/css/main.e699f326.chunk.css"
+  },
+  {
+    "revision": "2d1b3ccab08a652350bf",
+    "url": "/sortingapp/static/js/2.ddaad666.chunk.js"
+  },
+  {
+    "revision": "49bb9a2f9caa2c7a7f63",
+    "url": "/sortingapp/static/js/main.405f0b78.chunk.js"
+  },
+  {
+    "revision": "bde0155bd6a0b09d9b3f",
+    "url": "/sortingapp/static/js/runtime-main.d70b01ff.js"
+  }
+]);
